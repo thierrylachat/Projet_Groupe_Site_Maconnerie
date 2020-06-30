5618d8d3e43190497f7d9db48e63d2f586628454
clement-sabe <clem.sabe@gmail.com>
2020-06-30 16:47:31 +0200

    ajout 5 images AOS

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ document.getElementById('img3').setAttribute('data-aos','zoom-in');
 document.getElementById('img4').setAttribute('data-aos','zoom-in');
 document.getElementById('img5').setAttribute('data-aos','zoom-in');
 // AOS
-<<<<<<< HEAD
 // document.getElementById('img1').setAttribute('data-aos') = 'zoom-in';
 // document.getElementById('img1').style.borderColor = "red"
-=======
 
 document.getElementById('img1').setAttribute('data-aos','zoom-in');
->>>>>>> 3a8ca67427dbe67d038e29e90577abff45312bc3
