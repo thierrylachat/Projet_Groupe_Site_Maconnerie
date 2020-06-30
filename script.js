baf0f5339e459d1b263f27bb315685b21a5cdd8a
clement-sabe <clem.sabe@gmail.com>
2020-06-30 09:36:50 +0200

    ajout class et reglage button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const button = document.createElement("button");
 button.classList.add("btn");
+button.classList.add("btn-success");
+button.classList.add("mt-5");
+button.type = "submit";
 button.textContent="Envoyer";
 button.id = "btn";
 document.querySelector('form').appendChild(button);
