821a2825a7133f009d2e6c08c25276f23af56b5e
clement-sabe <clem.sabe@gmail.com>
2020-06-30 09:16:56 +0200

    création button en js

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.createElement("button");
+button.classList.add("btn");
+button.textContent="Envoyer";
+button.id = "btn";
+document.querySelector('form').appendChild(button);
+
+
