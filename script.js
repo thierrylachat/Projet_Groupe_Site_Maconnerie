2dadfdd8d94cefb886aa0e8d8329981f44c96e85
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-30 16:41:05 +0200

    MAJ.

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ button.id = "btn";
 document.querySelector('form').appendChild(button);
 
 // AOS
-document.getElementById('img1').setAttribute('data-aos') = 'zoom-in';
-document.getElementById('img1').style.borderColor = "red"
+
+document.getElementById('img1').setAttribute('data-aos','zoom-in');
