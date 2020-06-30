0dbdf6f19c217bedc9d858c39a81878f93d22181
clement-sabe <clem.sabe@gmail.com>
2020-06-30 16:29:50 +0200

    ajout AOS

--- index.php
+++ index.php
@@ -24,7 +24,8 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-b+jboW/YIpW2ZZYyYdXczKK6igHlnkPNfN9kYAbqYV7rNQ9PKTXlS2D6j1QZIATW" crossorigin="anonymous">
+    integrity="sha384-b+jboW/YIpW2ZZYyYdXczKK6igHlnkPNfN9kYAbqYV7rNQ9PKTXlS2D6j1QZIATW" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <!-- Affichage du titre en fonction de la page. -->
     <title><?= $file->page[$pageId]->title ?></title>
@@ -33,23 +34,25 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
 <body>
     <!-- Création de la barre de navigation. -->
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php?page=1/Accueil.html">Maçonnerie Ocordo</a>
+        <a class="navbar-brand" href="index.php?page=1/Accueil.html"><img class="logo" src="assets/img/Logo-Ocordo-Travaux-Nantes.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
             aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarColor02">
-        <!-- Création des liens de la barre de navigation en fonction des menus du fichier XML. -->
+            <!-- Création des liens de la barre de navigation en fonction des menus du fichier XML. -->
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item ">
-                    <a class="nav-link" href="index.php?page=1/Accueil.html"><?= $file->page[0]->menu ?><span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php?page=1/Accueil.html"><?= $file->page[0]->menu ?><span
+                            class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=2/Qui_sommes_nous.html"><?= $file->page[1]->menu ?></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=3/Nos_clients_témoignent.html"><?= $file->page[2]->menu ?></a>
+                    <a class="nav-link"
+                        href="index.php?page=3/Nos_clients_témoignent.html"><?= $file->page[2]->menu ?></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=4/Contact.html"><?= $file->page[3]->menu ?></a>
@@ -61,7 +64,10 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
     <?= $file->page[$pageId]->content ?>
 
     <!-- Liens Bootstrap. -->
+
     <script src="script.js"></script>
+    <!-- AOS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
@@ -71,5 +77,9 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
+    <script>
+        AOS.init();
+    </script>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -5,6 +5,13 @@ button.classList.add("mt-5");
 button.type = "submit";
 button.textContent="Envoyer";
 button.id = "btn";
-document.querySelector('form').appendChild(button);
+let form = document.querySelector('form');
+if (form) {
+    document.querySelector('form').appendChild(button);
+}
+
+
+// AOS
+document.getElementById('img1').setAttribute('data-aos','zoom-in');
 
 
