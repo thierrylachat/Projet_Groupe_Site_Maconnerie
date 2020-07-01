eff3fdf559b3eb2afe714c6fd4819a27108a72c5
clement-sabe <clem.sabe@gmail.com>
2020-07-01 11:05:06 +0200

    traitement formulaire

--- index.php
+++ index.php
@@ -15,7 +15,63 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
 // Affichage de la page d'accueil dés l'arrivée.
     $pageId = 0;
 }
-?>
+//validation du formulaire
+$isSubmitted = false;
+
+if (count($_POST) > 0) {
+    $isSubmitted = true;
+
+//declaration des variables
+
+$lastName = '';
+$mail = '';
+$phone = '';
+$subject = '';
+$city = '';
+$message = '';
+$errors = [];
+
+$regexPhone = '/^(+33|0)[1-79][0-9]{8}$/';
+$regexNames = '/^[a-zéèîïêëç]+((?:-|\s)[a-zéèéîïêëç]+)?$/i';
+$lastName = trim(filter_input(INPUT_POST, 'your-name', FILTER_SANITIZE_STRING));
+if (empty($lastName)) {
+    $errors['your-name'] = 'Merci de renseigner votre nom.';
+} elseif (!preg_match($regexNames, $lastName)) {
+    $errors['your-name'] = 'Le format attendu n\'est pas respecté.';
+}
+$mail = trim(filter_input(INPUT_POST, 'your-email', FILTER_SANITIZE_STRING));
+if (empty($mail)) {
+    $errors['your-email'] = 'Merci de renseigner correctement votre adresse électronique.';
+} elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    $errors['your-email'] = 'Le format attendu n\'est pas respecté.';
+}
+$phone = trim(filter_input(INPUT_POST, 'your-tel-615', FILTER_SANITIZE_STRING));
+if (empty($phone)) {
+    $errors['your-tel-615'] = 'Merci de renseigner correctement votre numéro de téléphone.';
+} elseif (!preg_match($regexPhone, $phone)) {
+    $errors['your-tel-615'] = 'Le format attendu n\'est pas respecté.';
+}
+$subject = trim(filter_input(INPUT_POST, 'your-subject', FILTER_SANITIZE_STRING));
+$city = trim(filter_input(INPUT_POST,'your-ville', FILTER_SANITIZE_STRING));
+if (empty($city)) {
+    $errors['your-ville'] = 'Merci de renseigner votre nom de ville.';
+} elseif (!preg_match($regexNames, $city)) {
+    $errors['your-ville'] = 'Le format attendu n\'est pas respecté.';
+}
+$message = trim(filter_input(INPUT_POST,'your-message', FILTER_SANITIZE_STRING));
+exit(json_encode($errors));
+}
+
+if($isSubmitted && count($errors) == 0): ?>
+
+    <div class="alert alert-success" role alert>
+        Votre compte a été créé avec succès <i class="far fa-grin-alt"></i>!!!
+    </div>
+    <?php endif; 
+
+    ?>
+
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -24,7 +80,7 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-b+jboW/YIpW2ZZYyYdXczKK6igHlnkPNfN9kYAbqYV7rNQ9PKTXlS2D6j1QZIATW" crossorigin="anonymous">
+        integrity="sha384-b+jboW/YIpW2ZZYyYdXczKK6igHlnkPNfN9kYAbqYV7rNQ9PKTXlS2D6j1QZIATW" crossorigin="anonymous">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <!-- Affichage du titre en fonction de la page. -->
@@ -34,7 +90,8 @@ if ((isset($_GET['page']) && ($_GET['page'] == 1))){
 <body>
     <!-- Création de la barre de navigation. -->
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php?page=1/Accueil.html"><img class="logo" src="assets/img/Logo-Ocordo-Travaux-Nantes.png" alt=""></a>
+        <a class="navbar-brand" href="index.php?page=1/Accueil.html"><img class="logo"
+                src="assets/img/Logo-Ocordo-Travaux-Nantes.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
             aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
